bf9a6e076f29b960e55f0e6f81df2ec11ba7fe53
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2022-07-09 17:12:12 +0200

    Created a form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,10 @@
 class UserController < ApplicationController
   def new
+    @users = User.all
     @user = User.new
+  end
+
+  def create
+    render plain: "Thanks"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get '/user', to: 'user#new'
+  post '/user', to: 'user#create'
   get '/tickets', to: 'tickets#index'
 end
