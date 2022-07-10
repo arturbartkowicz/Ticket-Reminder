1e521dc037f7da46485b96d22f52e743d515a4ce
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2022-07-10 20:15:11 +0200

    Create a form for creating tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,4 +1,34 @@
 class TicketsController < ApplicationController
-  def index
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    if @ticket.save
+      redirect_to root_path, notice: "#{@ticket.title} was successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @ticket = Ticket.find(params[:id])
+    @ticket.destroy
+
+    redirect_to root_path, status: :see_other, notice: "#{@ticket.title} was successfully deleted"
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(
+      :title,
+      :description,
+      :due_date,
+      :status_id,
+      :progress,
+      :user_id
+    )
   end
 end
